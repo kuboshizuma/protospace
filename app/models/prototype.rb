63a0db0fb08c1d13eb718b68838ff8146553264a
kubo shizuma <forothers.295@gmail.com>
2015-08-25 17:42:24 +0900

    fix some for page of tags

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,12 @@
 class Prototype < ActiveRecord::Base
   acts_as_taggable
+
   has_many :prototype_images
   belongs_to :user
+
   accepts_nested_attributes_for :prototype_images
+
   attr_reader :tag1, :tag2, :tag3
+
   validates :title, presence: true
 end
--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -1,6 +1,9 @@
 class PrototypeImage < ActiveRecord::Base
   self.inheritance_column = :_type_disabled
+
   belongs_to :prototype
+
   mount_uploader :image, ImageUploader
+
   validates_presence_of :image
 end
