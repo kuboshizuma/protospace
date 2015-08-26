0bb15302eda871a3318d42942db7118c99f3e2c3
kubo shizuma <forothers.295@gmail.com>
2015-08-26 20:36:31 +0900

    fix haml coding style and so on

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,7 +1,7 @@
 class Prototypes::NewestController < ApplicationController
 
   def index
-    @prototypes = Prototype.order('updated_at DESC').page(params[:page]).per(8).includes(:user, :prototype_images, :tags)
+    @prototypes = Prototype.order(updated_at: :desc).page(params[:page]).per(8).includes(:user, :prototype_images, :tags)
   end
 
 end
