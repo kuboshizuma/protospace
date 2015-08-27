4f3ee132dca7709d9c5af8116ffd960f36bee2fa
kubo shizuma <forothers.295@gmail.com>
2015-08-27 11:00:44 +0900

    fix some about prototype header and so on

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -6,17 +6,12 @@ class Prototype < ActiveRecord::Base
 
   accepts_nested_attributes_for :prototype_images, reject_if: :reject_image
 
-  validates_presence_of :title, :catch_copy, :concept
-  validate :must_have_one_captured_image
+  validates_presence_of :title, :catch_copy, :concept, :prototype_images
 
   paginates_per 8
 
   def reject_image(attributed)
     attributed[:name].blank?
-  end
-
-  def must_have_one_captured_image
-    errors.add(:prprptype_images, "can't be blank") if prototype_images.empty?
   end
 
 end
