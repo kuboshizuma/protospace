d59c19564f0653532736d28f78cef689fe176cb5
kubo shizuma <forothers.295@gmail.com>
2015-09-02 12:01:44 +0900

    [fix] divide function create and destroy

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,16 @@
 class LikesController < ApplicationController
   def create
-    like_status = params[:like_status]
     prototype_id = params[:prototype_id]
+    Like.create(prototype_id: prototype_id, user_id: current_user.id)
     all_count = Like.where(prototype_id: prototype_id).count
-    if like_status == "true"
-      like = Like.find_by(prototype_id: prototype_id, user_id: current_user.id)
-      like.destroy
-      render json: { all_count: all_count-1 }
-    else
-      Like.create(prototype_id: prototype_id, user_id: current_user.id)
-      render json: { all_count: all_count+1 }
-    end
+    render json: { all_count: all_count }
+  end
+
+  def destroy
+    prototype_id = params[:id]
+    like = Like.find_by(prototype_id: prototype_id, user_id: current_user.id)
+    like.destroy
+    all_count = Like.where(prototype_id: prototype_id).count
+    render json: { all_count: all_count }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :newest, only: [:index]
   end
   resources :prototypes, only: [:new, :create, :show]
-  resources :likes, only: [:create]
+  resources :likes, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
