0413a968e6e1bae3c9e63ccca2026465520dc9e1
kubo shizuma <forothers.295@gmail.com>
2015-09-07 14:05:40 +0900

    [fix] add cache_counter

--- app/controllers/prototypes/prototypes_controller.rb
+++ app/controllers/prototypes/prototypes_controller.rb
@@ -1,9 +1,7 @@
 class Prototypes::PrototypesController < ApplicationController
 
   def index
-    popular_ids = Prototype.joins('left join likes on prototypes.id = likes.prototype_id').group('prototypes.id').order('count_prototype_id desc').count('prototype_id').keys
-    @prototypes = Prototype.where(id: popular_ids).includes(:user, :prototype_images, :tags).index_by(&:id).slice(*popular_ids).values
-    @prototype_pagination = Kaminari.paginate_array(@prototypes).page(params[:page]).per(8)
+    @prototypes = Prototype.order(like_count: :desc).page(params[:page])
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831171430) do
+ActiveRecord::Schema.define(version: 20150907045738) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",         limit: 65535
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20150831171430) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
   end
-ActiveRecord::Schema.define(version: 20150831073459) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id",      limit: 4
@@ -44,8 +43,9 @@ ActiveRecord::Schema.define(version: 20150831073459) do
     t.string   "catch_copy", limit: 255
     t.text     "concept",    limit: 65535
     t.integer  "user_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.integer  "like_count", limit: 4,     default: 0
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
