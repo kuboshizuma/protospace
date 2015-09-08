07cc474bca49ea48af6695853d1b44882dcafa3e
kubo shizuma <forothers.295@gmail.com>
2015-09-08 10:50:33 +0900

    [fix] fix some

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,8 +1,8 @@
 $(function(){
   $('.img_preview').each(function(){
-    var selfInput = $(this).find('input[type=file]');
-    selfInput.change(function(){
-      var file = selfInput[0].files[0];
+    var $selfInput = $(this).find('input[type=file]');
+    $selfInput.change(function(){
+      var file = $selfInput[0].files[0];
       $img = $(this).siblings('img')
       fileRdr = new FileReader();
       fileRdr.onload = function(event) {
@@ -15,10 +15,10 @@ $(function(){
 });
 
 $(function(){
-  var last_image_input = $('.list-group-item:nth-child(3)')
-  last_image_input.css('display', 'none');
+  var $lastImageInput = $('.list-group-item:nth-child(3)')
+  $lastImageInput.css('display', 'none');
   $('.list-group-item-plus').on('click', function(){
     $(this).css('display', 'none');
-    last_image_input.css('display', 'block');
+    $lastImageInput.css('display', 'block');
   });
 });
