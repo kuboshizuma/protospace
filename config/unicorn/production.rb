cfa8d54380a1d9ebd13e4bf74ec591ddcabf3b5e
kubo shizuma <forothers.295@gmail.com>
2015-11-15 17:52:10 +0900

    Add for capistrano

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,9 +1,10 @@
 $worker  = 2
 $timeout = 30
-$app_dir = "/var/www/rails/protospace/shared"
-$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path 'log/unicorn.log', $app_dir
+$app_dir = "/var/www/rails/protospace/current"
+$share_dir = "/var/www/rails/protospace/current"
+$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $share_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $share_dir
+$std_log = File.expand_path 'log/unicorn.log', $share_dir
 # set config
 worker_processes  $worker
 working_directory $app_dir
